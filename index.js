1ac9dec41cf174fe5bb32cbb17ca08a5cd3dc756
Trisharawat05 <148178489+Trisharawat05@users.noreply.github.com>
2024-12-12 09:09:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Add any JS functionality if needed. For now, this script logs that the page loaded.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Page Loaded");
+});
+
+
