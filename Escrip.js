a80013b5ca487b4f09f12460f6e31dca78d5a670
Trisharawat05 <148178489+Trisharawat05@users.noreply.github.com>
2025-01-08 22:12:32 +0530

    Delete Escrip.js

--- Escrip.js
+++
@@ -1,9 +0,0 @@
- Script for button actions
-document.querySelector('.contribute-btn').addEventListener('click', function() {
-    alert("Thank you for considering a contribution! Redirecting you to the donation page...");
-});
-
-document.querySelector('.evacuation-btn').addEventListener('click', function() {
-    alert("Redirecting to the Evacuation Map...");
-});
-
