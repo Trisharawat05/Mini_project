0085b519fe68edc5c26a38af1b2801da534fed8e
Trisharawat05 <148178489+Trisharawat05@users.noreply.github.com>
2025-01-08 22:13:10 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// Add any JS functionality if needed. For now, this script logs that the page loaded.
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Page Loaded");
-});
-
-
